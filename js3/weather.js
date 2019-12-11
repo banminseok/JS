69e753a9a8ce5bff0690e1b1625d0983b5f2a2dd
banminseok <halfmin@gmail.com>
2019-12-11 23:42:44 +0900

    commit on

---
+++ js3/weather.js
@@ -0,0 +1,39 @@
+const COORDS = "coords";
+const API_KEY="1dfba7be46de741d46b698a08142fc69";
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    //console.log(position)
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    }
+    saveCoords(coordsObj);
+}
+function handleGeoError(){
+    console.log('Cant access geo location');
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess,handleGeoError);
+}
+
+function loadCoords(){
+    const loadCoords = localStorage.getItem(COORDS);
+    if (loadCoords === null){
+        askForCoords();       
+    }else{
+
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
